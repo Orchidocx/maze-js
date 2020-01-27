74dcad00e313c59b85c80390176497892b3ec83a
Jacky Li <ilikemacaronii@gmail.com>
2020-01-26 22:36:33 -0800

    incomplete refactor

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const {World, Engine, Runner, Render, Bodies} = Matter;
-const width = 600, height = 600;
-const gridSize = 6;
-const unitLength = width/gridSize;
+const {World, Engine, Runner, Render, Bodies, Body, Events} = Matter;
+const width = window.innerWidth, height = window.innerHeight;
+const gridWidth = 6, gridHeight = 6;
+const unitLengthX = width/gridWidth;
+const unitLengthY = height/gridHeight;
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const {world} = engine;
 const render = Render.create({
     element: document.body,
@@ -19,10 +21,10 @@ Runner.run(Runner.create(), engine);
 
 // Walls
 const walls = [
-    Bodies.rectangle(width/2,0, width, 40, {isStatic: true}),
-    Bodies.rectangle(width/2, height, width, 40, {isStatic: true}),   
-    Bodies.rectangle(0, height/2, 40, height, {isStatic: true}),
-    Bodies.rectangle(width, height/2, 40, height, {isStatic: true})
+    Bodies.rectangle(width/2,0, width, 5, {isStatic: true}),
+    Bodies.rectangle(width/2, height, width, 5, {isStatic: true}),   
+    Bodies.rectangle(0, height/2, 5, height, {isStatic: true}),
+    Bodies.rectangle(width, height/2, 5, height, {isStatic: true})
 ]
 World.add(world, walls);
 
@@ -97,10 +99,76 @@ const addWalls = (arr, orientation) => {
                 h = unitLength;
                 
             }
-            const wall = Bodies.rectangle(xPos, yPos, w, h, { isStatic:true });
+            const wall = Bodies.rectangle(xPos, yPos, w, h, 
+                { 
+                    isStatic:true, 
+                    label: 'wall'
+                });
             World.add(world, wall);
         });
     });
 }
 addWalls(horizontals, "horizontal");
 addWalls(verticals, "vertical");
+
+// Goal
+const goal = Bodies.rectangle(width - unitLength/2, height - unitLength/2, unitLength/2, unitLength/2,
+    {
+        isStatic: true,
+        label: 'goal'
+    });
+World.add(world, goal);
+
+// Player
+const player = Bodies.circle(
+    unitLength/2,
+    unitLength/2,
+    unitLength/4,
+    {
+        restitution: .25,
+        label: 'player'
+    }
+);
+World.add(world, player);
+
+document.addEventListener('keydown', event => {
+    const {x,y} = player.velocity;
+    switch(event.keyCode) {
+        case 38:
+        case 87:
+            Body.setVelocity(player, {x, y: y - 5});
+            break;
+        case 39:
+        case 68:
+            Body.setVelocity(player, {x: + 5, y});
+            break;
+        case 40:
+        case 83:
+            Body.setVelocity(player, {x, y: y + 5});
+            break;
+        case 37:
+        case 65:
+            Body.setVelocity(player, {x: - 5, y});
+            break;
+        case 32:
+            console.log("space jump!");
+        default:
+            console.log(event.keyCode);
+    }
+});
+
+// Win Condition
+Events.on(engine, 'collisionStart', event => {
+    event.pairs.forEach((collision) => {
+        const labels = ['player', 'goal'];
+        if(labels.includes(collision.bodyA.label) &&
+           labels.includes(collision.bodyB.label)) {
+               world.gravity.y = 1;
+               world.bodies.forEach(body => {
+                   if(body.label === 'wall') {
+                       Body.setStatic(body, false);
+                   }
+               })
+           }
+    })
+})
